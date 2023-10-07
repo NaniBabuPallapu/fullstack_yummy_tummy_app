6d7e38f03c2402124ac5c5d2c9612c0aac9e8e55
Nani_Babu <nanibabu.pallapu@innovasolutions.com>
2023-10-07 17:40:42 +0530

    initial commit

---
+++ angular-yummy-tummy/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserCreateComponent,
+    UserListComponent,
+    UserDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
