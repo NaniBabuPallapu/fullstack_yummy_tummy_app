d98a2ed6d8d81206bae435cc64d9abfc556d1e7e
NaniBabuPallapu <nanipallapu369@gmail.com>
2023-10-10 15:12:08 +0530

    adding files

--- angular-yummy-tummy/src/app/components/user-list/user-list.component.ts
+++ angular-yummy-tummy/src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { API_BASE_URL } from 'src/app/constants/app.constants';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+
+  usersList! : User[];
+
+  constructor(private httpClient : HttpClient, private userService : UserService){
+
+  }
+
+  ngOnInit(){
+
+    this.getListOfUsers();
+    console.log("List of users" + this.usersList);
+
+
+
+  }
+
+  getListOfUsers(){
+    this.userService.getAllUsers().subscribe(data => this.usersList = data);
+  }
 
 }
