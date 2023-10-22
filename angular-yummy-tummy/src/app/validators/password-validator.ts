fae97e8b2bfda8434e6b99dfce9f487b336f13ba
NaniBabuPallapu <nanipallapu369@gmail.com>
2023-10-22 12:58:20 +0530

    creating user profile css

--- angular-yummy-tummy/src/app/validators/password-validator.ts
+++ angular-yummy-tummy/src/app/validators/password-validator.ts
@@ -4,7 +4,7 @@ export function PasswrodValidator(control: AbstractControl) {
     if (control && control.value && (control.value === null)&& control.value==='') {
         return null
     }
-    if (control.value.length >= 4 && control.value.length <= 10) {
+    if (control.value.length >= 4 && control.value.length <= 15) {
         return null; // Don't show the error until at least 10 characters are entered
     }
     return {
